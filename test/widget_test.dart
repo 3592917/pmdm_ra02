f353108796266a4f3c78f50b818bbbc5feff1f34
3592917 <3592917@alu.murciaeduca.es>
2025-10-21 23:35:02 +0200

    Dependencies updated and new tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,20 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pmdm_ra02/my_app.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('MyHomePage UI Test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that the title is displayed.
+    expect(find.text('Flutter básico - HTTP y Serialización'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that all buttons are displayed.
+    expect(find.widgetWithText(ElevatedButton, 'Obtener usuario'), findsOneWidget);
+    expect(find.widgetWithText(ElevatedButton, 'Obtener todos los usuarios'), findsOneWidget);
+    expect(find.widgetWithText(ElevatedButton, 'Crear un nuevo usuario'), findsOneWidget);
+    expect(find.widgetWithText(ElevatedButton, 'Modificar usuario'), findsOneWidget);
+    expect(find.widgetWithText(ElevatedButton, 'Borrar usuario'), findsOneWidget);
   });
 }
